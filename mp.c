c0a270527b5dc96bef5a8d3753c61238fe1f7a52
EgorBeschetnov <0e.besch0@gmail.com>
2022-02-25 13:43:17 +0700

    input clang-format

---
+++ mp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char input[64];
+	printf("input figure\n");
+	scanf("%s",&input);
+
+	if(strcmp(input,"circle") == 0){
+		printf("you input circle\n");}
+
+return 0;
+}
