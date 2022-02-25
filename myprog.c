6df44063cb7386b223d46761b54ddae36532c508
EgorBeschetnov <0e.besch0@gmail.com>
2022-02-25 12:19:55 +0700

    Create function

---
+++ myprog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char input[64];
+	printf("Введите фигуру\n");
+	scanf("%s",&input);
+
+	if(strcmp(input, "circle") == 0){
+		printf("Вы ввели круг\n");
+	}
+return 0;
+}
